d1303c184672fa96ee236b70e026f51adecfd7cf
yungProg <122945393+yungProg@users.noreply.github.com>
2025-05-18 21:49:42 +0000

    Complete test 8

--- spec/08_change_spec.rb
+++ spec/08_change_spec.rb
@@ -55,15 +55,18 @@ describe 'lucky numbers with rotate! method' do
 
   context 'when rotating the array of lucky numbers' do
     # remove the 'x' before running this test
-    xit 'will change the first value to 7' do
+    it 'will change the first value to 7' do
+      expect { lucky_numbers.rotate! }.to change { lucky_numbers.first }.from(3).to(7)
     end
 
     # remove the 'x' before running this test
-    xit 'will change the last value to 3' do
+    it 'will change the last value to 3' do
+      expect { lucky_numbers.rotate! }.to change { lucky_numbers.last }.to(3)
     end
 
     # remove the 'x' before running this test
-    xit 'will change the first value to 7 and last value to 3' do
+    it 'will change the first value to 7 and last value to 3' do
+      expect { lucky_numbers.rotate! }.to change { lucky_numbers.first }.to(7).and change { lucky_numbers.last }.to(3)
     end
   end
 end
