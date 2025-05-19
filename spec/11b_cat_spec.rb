5e8df2a0362805ee3c2dac62beb7a8718816c0f1
yungProg <gboakye007@outlook.com>
2025-05-19 18:19:54 +0000

    Complete test 11b

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -36,16 +36,22 @@ end
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
 
+  subject(:pyne) { described_class.new('Pyne')}
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when method is shared among various classes' do
+    it 'is expected to respond to talk' do
+      expect(pyne).to  respond_to(:talk)
+    end
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(pyne).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(pyne).to be_hiding
   end
 end
